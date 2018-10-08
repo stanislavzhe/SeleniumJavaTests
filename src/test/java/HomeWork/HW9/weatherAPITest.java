088c7120e90da91d9364de28bf9241066b326475
stanislav.zheleznov <stanislav.zheleznov@x2sy.com>
2018-10-08 18:11:17 +0300

    new commit

--- src/test/java/HomeWork/HW9/weatherAPITest.java
+++ src/test/java/HomeWork/HW9/weatherAPITest.java
@@ -17,7 +17,6 @@ public class weatherAPITest {
 //    2. В качестве группы тестов/теста добавить сравнение полученных данных с weather.com
 //    (для получения данных с сайта использовать selenium)
 //    3. Сравнить температуру воздуха из API и на сайте
-//    test for git
 
     @Test
     public void apiTest() {
